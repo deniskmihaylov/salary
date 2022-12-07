fc37ac334b39b832bd5650d99c9894e1e6a2fcb5
Denis Mihaylov <116539046+deniskmihaylov@users.noreply.github.com>
2022-12-07 23:59:20 +0000

    Add files via upload

---
+++ salary.js
@@ -0,0 +1,30 @@
+function solve(input) {
+  let openTabs = Number(input[0]);
+  let salary = Number(input[1]);
+
+  for (let i = 2; i <= input.length; i++) {
+    let website = input[i];
+
+    if (website === "Facebook") {
+      salary -= 150;
+    } else if (website === "Instagram") {
+      salary -= 100;
+    } else if (website === "Reddit") {
+      salary -= 50;
+    } if (salary <= 0) {
+        console.log(`You have lost your salary.`); 
+        return;
+    } 
+    } if (salary > 0) {
+        console.log(`${salary}`)
+    }
+  }
+
+
+solve (["3",
+"500",
+"Github.com",
+"Stackoverflow.com",
+"softuni.bg"])
+
+
